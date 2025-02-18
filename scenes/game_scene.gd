473e4dd87cf5fdb9c2f8e8775d36fb54efea2e37
Marek Belski <marek.belski@gmail.com>
2025-02-17 20:32:01 -0800

    Detect when level is complete.

--- scenes/game_scene.gd
+++ scenes/game_scene.gd
@@ -1,11 +1,16 @@
 extends Node
 
+signal level_complete
+
 @export var document_scene : PackedScene
 @export var documents : Array[DocumentData]
 
-var _highlighted_document : Node2D
-var _active_document : Node2D
+var _highlighted_document : DocumentBase
+var _active_document : DocumentBase
 var _documents : Array[DocumentData]
+var _archived_documents : Array[DocumentData]
+var _redacted_documents : Array[DocumentData]
+var _processed_documents : Array[DocumentData]
 
 var _last_mouse_position : Vector2
 var points_scored : int = 0 :
@@ -32,7 +37,8 @@ func pickup_highlighted_document():
 	if is_instance_valid(_active_document):
 		return
 	_active_document = _highlighted_document
-	if _active_document == null : return
+	if _active_document == null : 
+		return
 	_highlighted_document = null
 	_active_document.reparent(%ActiveContainer)
 	_update_active_document_position()
@@ -42,19 +48,34 @@ func _update_active_document_position():
 		return
 	_active_document.position = _last_mouse_position
 
+func is_level_complete():
+	return _processed_documents.size() >= documents.size()
+
+func _finish_level_if_complete():
+	if is_level_complete():
+		print("LEVEL COMPLETE")
+		level_complete.emit()
+
 func drop_document():
 	if not is_instance_valid(_active_document):
 		return
 	if _active_document is DocumentBase:
 		if %Outbox2D.is_mouse_over or %Furnace2D.is_mouse_over:
+			_processed_documents.append(_active_document.document_data)
+			if %Furnace2D.is_mouse_over:
+				_redacted_documents.append(_active_document.document_data)
+			else:
+				_archived_documents.append(_active_document.document_data)
 			if _active_document.document_data.is_dangerous == %Furnace2D.is_mouse_over:
 				points_scored += 1
 			else:
 				points_scored -= 1
 			_active_document.queue_free()
 			_active_document = null
+			_finish_level_if_complete()
 			return
 	_active_document.reparent(%Container)
+	_highlighted_document = _active_document
 	_active_document = null
 
 func _on_inbox_ui_pressed():
