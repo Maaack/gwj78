ee50698e24d62d89a42bdea40fd59172cb409972
vikfr0 <frovikfro@gmail.com>
2025-02-22 18:29:30 +0100

    lots of content

--- resources/document_data.gd
+++ resources/document_data.gd
@@ -4,6 +4,9 @@ extends Resource
 @export var is_processable: bool = true
 @export var is_messageable: bool = false
 @export var is_dangerous : bool = false
+@export var is_resistance : bool = false
 @export var special_flags: Array[String] = []
 @export var document_scenes: Array[PackedScene] = []
 @export_multiline var content: String
+@export_multiline var content_page_2: String
+@export var triggers_dialogue: StringName
--- scenes/documents/_document_base.gd
+++ scenes/documents/_document_base.gd
@@ -6,6 +6,7 @@ signal mouse_entered
 signal mouse_exited
 
 var is_mouse_over : bool = false
+var is_turned : bool = false
 
 @export var document_data : DocumentData :
 	set(value):
@@ -17,14 +18,32 @@ var is_mouse_over : bool = false
 
 func _ready():
 	document_data = document_data
-	
+
+
 func get_bounds() -> RectangleShape2D:
 	var rect = %CollisionShape2D.shape as RectangleShape2D
 	return rect
-	
+
+
 func play_pickup_sound() -> void:
 	if %OpenedStreamPlayer2D.playing : return
 	%OnMoveShuffleSound.play()
+
+
+func has_second_page() -> bool:
+	return document_data.content_page_2 != null and not document_data.content_page_2.is_empty()
+
+
+func turn_page() -> void:
+	if not has_second_page():
+		return
+		
+	is_turned = !is_turned
+	if is_turned:
+		%DocumentBody.text = document_data.content_page_2
+	else:
+		%DocumentBody.text = document_data.content
+
 
 func _on_area_2d_mouse_entered():
 	if disabled: return
